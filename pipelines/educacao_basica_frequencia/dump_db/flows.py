5a40762b34677d42ed4a05befb4eeac50f938adb
KarinaPassos <kaadrain@gmail.com>
2024-01-15 16:35:00 -0300

    setting unique name for flow

--- pipelines/educacao_basica_frequencia/dump_db/flows.py
+++ pipelines/educacao_basica_frequencia/dump_db/flows.py
@@ -3,21 +3,20 @@
 Database dumping flows for SME project.
 """
 
-import prefeitura_rio
-
 from copy import deepcopy
 
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
-
-from pipelines.constants import constants
-
-from pipelines.educacao_basica.dump_db.schedules import sme_educacao_basica_daily_update_schedule
 from prefeitura_rio.pipelines_templates.dump_db.flows import flow as dump_sql_flow
 from prefeitura_rio.pipelines_utils.prefect import set_default_parameters
 
+from pipelines.constants import constants
+from pipelines.educacao_basica.dump_db.schedules import (
+    sme_educacao_basica_daily_update_schedule,
+)
+
 dump_sme_flow = deepcopy(dump_sql_flow)
-dump_sme_flow.name = "SME: educacao_basica - Ingerir tabelas de banco SQL"
+dump_sme_flow.name = "SME: educacao_basica_frequencia - Ingerir tabelas de banco SQL"
 dump_sme_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 
 dump_sme_flow.run_config = KubernetesRun(
@@ -35,8 +34,6 @@ sme_default_parameters = {
     "vault_secret_path": "clustersqlsme",
     "dataset_id": "educacao_basica_frequencia",
 }
-dump_sme_flow = set_default_parameters(
-    dump_sme_flow, default_parameters=sme_default_parameters
-)
+dump_sme_flow = set_default_parameters(dump_sme_flow, default_parameters=sme_default_parameters)
 
 dump_sme_flow.schedule = sme_educacao_basica_daily_update_schedule

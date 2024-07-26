d7bab770a6d0202d082b5eae13eea3eaad1fc04f
d116626 <d116626@gmail.com>
2024-07-26 17:58:36 -0300

    feat: add upstream for all tasks in dump_db

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Imports all flows for every project so we can register all of them..
+Imports all flows for every project so we can register all of them...
 """
 from pipelines.educacao_basica import *  # noqa
 from pipelines.educacao_basica_frequencia import *  # noqa
